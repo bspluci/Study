7417a579d438ae01e681d6e949c013bcde6ee2b5
pjh <pjh@gmail.com>
2019-12-10 10:27:01 +0900

    aa

---
+++ HDstudy/js/custom.js
@@ -0,0 +1,91 @@
+$(document).ready(function() {
+   // 맴버정보 더보기 이벤트
+   $.ajax({
+      url: "./json/member.json",
+      type: "GET",
+      dataType: "JSON",
+      success: function(data) {
+         for (i = 0; i < data.length; i++) {
+            var temp = `<li class="mb_list hidden">
+                        <div class="mb_imgbox">
+                           <img src="${data[i].img}" />
+                        </div>
+                        <div class="mb_txtbox">
+                           <p><span>name :</span> ${data[i].name}</p>
+                           <p><span>age :</span> ${data[i].age}</p>
+                           <p><span>gender :</span> ${data[i].gender}</p>
+                           <p><span>place :</span> ${data[i].place}</p>
+                           <p class="mb_intro"><span>intro :</span> ${data[i].intro}</p>
+                        </div>
+                     </li>`;
+            $(".mb_wrap").append(temp);
+         }
+      },
+      error: function() {
+         alert("데이터 로드에 실패했습니다.");
+      }
+   });
+
+   $(".more_btn").click(function() {
+      var len = $(".hidden").length;
+      if (!len <= 0) {
+         $(".hidden")
+            .eq(1)
+            .removeClass("hidden")
+            .addClass("mb_slideup");
+         $(".hidden")
+            .eq(0)
+            .removeClass("hidden")
+            .addClass("mb_slideup");
+      } else {
+         alert("더이상 표시할 내용이 없습니다.");
+      }
+   });
+
+   // 슬라이트 터치스와이프 이벤트
+   // $('.page_slide_box').cfTouchSwipe({
+   //			minSwipeLength: 50,		// 사용자가 swipe 했다고 판단하는 최소 거리
+   //			minMoveLength: 15,		// 이 거리보다 작게 움직이고 triggerClick = true 이면 click 이벤트 trigger
+   //			triggerClick: true,		// triggerClick false 일 경우 click 이벤트가 발생하지 않음
+   //			preventDefault: true,		// touchstart, touchmove 이벤트에 대해 preventDefault 할지 여부
+   // });
+   $(".slider_box").cfTouchSwipe({
+      triggerClick: false,
+      swipeLeft: function() {
+         $(".slider_nextbtn").trigger("click");
+      },
+      swipeRight: function() {
+         $(".slider_prevbtn").trigger("click");
+      }
+   });
+
+   // 스무스 스크롤 이벤트
+   $("body, html").bind("mousewheel", function(e) {
+      var top = parseInt($("#section").offset().top),
+         SCTOP = 350;
+      if (!(e.originalEvent.wheelDelta == 120)) {
+         $("#section").css("top", top - SCTOP + "px");
+
+         var secHeight = $("#section").height();
+         var winHeight = $(window).height();
+
+         myHeight = winHeight - secHeight;
+
+         if (top <= myHeight + SCTOP) {
+            // setTimeout(function() {
+            $("#section").css("top", myHeight);
+            // }, 100);
+         }
+
+         windowScroll();
+      } else {
+         $("#section").css("top", top + SCTOP + "px");
+
+         if (top >= -SCTOP) {
+            // setTimeout(function() {
+            $("#section").css("top", "0");
+            // }, 100);
+         }
+      }
+   });
+});
--- javascript/painting/app.js
+++ javascript/painting/app.js
@@ -1,12 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.querySelectorAll(".jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
-canvas.width = 700;
-canvas.height = 700;
-ctx.strokeStyle = "#2c2c2c";
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
    painting = false;
@@ -28,8 +37,31 @@ function onMouseMove(event) {
    }
 }
 
-function onMouseDown(event) {
-   painting = true;
+function handleColorClick(event) {
+   const color = event.target.style.backgroundColor;
+   ctx.strokeStyle = color;
+   ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+   const size = event.target.value;
+   ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+   if (filling == true) {
+      filling = false;
+      mode.innerHTML = "Fill";
+   } else {
+      filling = true;
+      mode.innerHTML = "Paint";
+   }
+}
+
+function handleCanvasClick() {
+   if (filling) {
+      ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+   }
 }
 
 if (canvas) {
@@ -37,4 +69,18 @@ if (canvas) {
    canvas.addEventListener("mousedown", startPainting);
    canvas.addEventListener("mouseup", stopPainting);
    canvas.addEventListener("mouseleave", stopPainting);
+   canvas.addEventListener("click", handleCanvasClick);
+}
+
+// colors.forEach(color => color.addEventListener("click", handleColorClick));
+colors.forEach(function() {
+   this.addEventListener("click", handleColorClick);
+});
+
+if (range) {
+   range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+   mode.addEventListener("click", handleModeClick);
 }
